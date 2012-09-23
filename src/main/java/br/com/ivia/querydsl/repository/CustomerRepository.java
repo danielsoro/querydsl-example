0a78b98812576ed9fb3479fbdf0a9c047557bba0
Daniel Cunha <danielsoro@gmail.com>
2012-09-23 11:10:05 -0300

    Adição do método salvar.
    Adição do método salvar para o repositório do Customer.

--- src/main/java/br/com/ivia/querydsl/repository/CustomerRepository.java
+++ src/main/java/br/com/ivia/querydsl/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import br.com.ivia.querydsl.model.Customer;
 
 public interface CustomerRepository {
 	Customer findByFirstName(String firstName);
+	Customer save(Customer customer);
 }
--- src/main/java/br/com/ivia/querydsl/repository/CustomerRepositoryImpl.java
+++ src/main/java/br/com/ivia/querydsl/repository/CustomerRepositoryImpl.java
@@ -2,6 +2,8 @@ package br.com.ivia.querydsl.repository;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.ivia.querydsl.model.Customer;
 import br.com.ivia.querydsl.model.QCustomer;
@@ -10,6 +12,10 @@ import com.mysema.query.jpa.impl.JPAQuery;
 
 @Stateless
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
 	@Inject
 	private JPAQuery jpaQuery;
 
@@ -17,7 +23,14 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 	public Customer findByFirstName(String firstName) {
 		QCustomer customer = QCustomer.customer;
 
-		return jpaQuery.from(customer).where(customer.firstName.equalsIgnoreCase(firstName))
+		return jpaQuery.from(customer)
+				.where(customer.firstName.equalsIgnoreCase(firstName))
 				.uniqueResult(customer);
+	}
+
+	@Override
+	public Customer save(Customer customer) {
+		em.persist(customer);
+		return customer;
 	}
 }
