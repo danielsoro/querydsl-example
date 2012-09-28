6eca35593777a6643a02f63c1b4378562325ea48
Daniel Cunha <danielsoro@gmail.com>
2012-09-28 01:00:40 -0300

    Refactor das fachadas para atender nova estrutura de repository
    Feita alteração da fachada para pegar instância do repository e
    retornar os DTOs.

--- src/main/java/br/com/ivia/querydsl/fachada/FachadaQueryDslImpl.java
+++ src/main/java/br/com/ivia/querydsl/fachada/FachadaQueryDslImpl.java
@@ -1,38 +1,24 @@
 package br.com.ivia.querydsl.fachada;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 
-import br.com.ivia.querydsl.model.QCustomer;
+import br.com.ivia.querydsl.model.Customer;
 import br.com.ivia.querydsl.model.dto.CustomerDTO;
-
-import com.mysema.query.jpa.impl.JPAQuery;
-import com.mysema.query.types.ConstructorExpression;
+import br.com.ivia.querydsl.repository.CustomerRepository;
 
 @Stateless
 public class FachadaQueryDslImpl implements FachadaQueryDsl {
-
-	@Inject
-	private JPAQuery jpaQuery;
-
+	
+	@EJB
+	private CustomerRepository customerRepository;
+	
 	@Override
 	public CustomerDTO findCustomerByFirstName(String firstName) {
-		QCustomer customer = QCustomer.customer;
-		return jpaQuery
-				.from(customer)
-				.where(customer.firstName.equalsIgnoreCase(firstName))
-				.uniqueResult(
-						ConstructorExpression.create(CustomerDTO.class,
-								customer.firstName));
+		Customer customer = customerRepository.findByFirstName(firstName);
+		
+		if (customer != null)
+			return new CustomerDTO(customer.getFirstName());
+		return null;
 	}
-
-	@Override
-	public CustomerDTO fidCustomerWithPhoneByFirstName(String firstName) {
-		QCustomer customer = QCustomer.customer;
-
-		return jpaQuery.from(customer).uniqueResult(
-				ConstructorExpression.create(CustomerDTO.class,
-						customer.firstName,customer.phone.number));
-	}
-
 }
