64c151837aeabc4a805936197f4127ae4b1f7817
Daniel Cunha <danielsoro@gmail.com>
2012-09-28 01:02:57 -0300

    Criação de teste para CustomerRepository
    Criação de teste para CustomerRepository

---
+++ src/test/java/br/com/ivia/querydsl/repository/CustomerRepositoryTest.java
@@ -0,0 +1,48 @@
+package br.com.ivia.querydsl.repository;
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.ivia.querydsl.fachada.FachadaQueryDsl;
+import br.com.ivia.querydsl.model.Customer;
+import br.com.ivia.querydsl.model.dto.CustomerDTO;
+import br.com.ivia.querydsl.view.CustomerDtoBean;
+
+@RunWith(Arquillian.class)
+public class CustomerRepositoryTest {
+
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap
+				.create(WebArchive.class, "test.war")
+				.addPackages(true, Customer.class.getPackage(),
+						CustomerRepository.class.getPackage(),
+						FachadaQueryDsl.class.getPackage(),
+						CustomerDTO.class.getPackage(),
+						CustomerDtoBean.class.getPackage())
+				.addAsResource("META-INF/test-persistence.xml",
+						"META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				// Deploy our test datasource
+				.addAsWebInfResource("test-ds.xml", "test-ds.xml")
+				.addAsLibraries(
+						DependencyResolvers.use(MavenDependencyResolver.class)
+								.includeDependenciesFromPom("pom.xml")
+								.resolveAsFiles());
+	}
+
+	@Test
+	public void testarJunit() {
+		Assert.assertTrue(true);
+	}
+}
